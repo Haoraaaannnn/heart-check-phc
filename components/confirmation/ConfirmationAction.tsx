4a6a1804fce1ddc8d73f774efa8c16cb9099683e
NejChoco <171265952+NejChoco@users.noreply.github.com>
2026-03-25 03:07:21 +0800

    updates ata itu

--- components/confirmation/ConfirmationAction.tsx
+++ components/confirmation/ConfirmationAction.tsx
@@ -1,22 +1,23 @@
 import Link from "next/link";
+import { Service } from "@/types/Services";
 
 interface Props {
   serviceId: number;
   serviceColor: string;
 }
 
-export default function ConfirmationActions({ serviceId, serviceColor}: Props) {
+export default function ConfirmationActions({ serviceId, serviceColor }: Props) {
   return (
     <div className="flex flex-col justify-center mb-8">
       <Link
         href={`/kiosk/sms-input?serviceId=${serviceId}`}
-        className="mx-8 my-3 max-w-full py-[18px] px-[18px] text-white text-center text-[60px] font-baloo font-black rounded-[45px] transition-all active:scale-90 shadow-xl"
-        style={{background:serviceColor}}>
+        className="mx-8 my-3 max-w-full py-[18px] px-[18px] bg-red-600 text-white text-center text-[60px] font-baloo font-black rounded-[45px] transition-all active:scale-90 shadow-xl"
+        style={{background: serviceColor}}>
         Magpatuloy - Continue
       </Link>
       <Link
         href="/kiosk/kiosk-services"
-        className="mx-8 my-3 max-w-full py-[18px] px-[18px] border-[6px] border-gray-400 font-baloo font-black text-center rounded-[45px] text-gray-400 text-[50px] transition-all active:scale-90"
+        className="mx-8 my-3 max-w-full py-[18px] px-[18px] border-gray-400 border-[4px] text-center rounded-[45px] text-gray-800 text-[50px] transition-all active:scale-90"
       >
         Bumalik - Cancel
       </Link>
