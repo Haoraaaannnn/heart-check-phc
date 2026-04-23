56063437fca9c1232d71cab379ff77b1696c4acc
NejChoco <171265952+NejChoco@users.noreply.github.com>
2026-04-23 15:09:18 +0800

    fix soe ui

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  //* insert the ip address from the npm run dev
+  allowedDevOrigins: ['198.168.92.168']
 };
 
 export default nextConfig;
