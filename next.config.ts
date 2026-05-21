83ab7fd6361e00a2651f52377cda0a26b422a878
NejChoco <171265952+NejChoco@users.noreply.github.com>
2026-05-21 22:54:30 +0800

    test run and update on laptop

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   //* insert the ip address from the npm run dev
-  allowedDevOrigins: ['198.168.92.168']
+  allowedDevOrigins: ['198.168.50.242']
 };
 
 export default nextConfig;
